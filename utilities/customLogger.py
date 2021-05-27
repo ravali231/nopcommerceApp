ba95d2c113c50c94c302a8002eb4fd5faa9dcade
ravali231 <ravalireddy231@gmail.com>
2021-05-27 04:24:07 -0400

    adding permissions w

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -5,9 +5,9 @@ class LogGen:
     def loggen():
         for handler in logging.root.handlers[:]:
             logging.root.removeHandler(handler)
-        logging.basicConfig(filename="/home/ravali/PycharmProjects/nopcommerceApp/Logs/Automation2.log",
+        logging.basicConfig(filename="/home/ravali/PycharmProjects/nopcommerceApp/Logs/Automation3.log",
                             format='%(asctime)s:%(levelname)s:%(message)s',
-                            datefmt='%m/%d/%Y %I:%M:%S:%P',filemode="a+")
+                            datefmt='%m/%d/%Y %I:%M:%S:%P',filemode="w+")
 
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)

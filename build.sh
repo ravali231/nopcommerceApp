c52087f1d2f2af8f919c753863cef8fa6653b806
ravali231 <ravalireddy231@gmail.com>
2021-05-27 17:05:06 +0530

    adding permissions w

--- build.sh
+++ build.sh
@@ -4,4 +4,8 @@ source venv/bin/activate
 pip3 install pytest
 pip3 install -U setuptools pip
 pip3 install --user numpy
+wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+tar -xvzf geckodriver
+chmod +x geckodriver
+sudo mv geckodriver /usr/local/bin/
 
